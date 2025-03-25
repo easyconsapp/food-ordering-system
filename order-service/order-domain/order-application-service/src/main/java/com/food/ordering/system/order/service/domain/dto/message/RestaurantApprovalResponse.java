8a4f41f6b2dffbe9e9a74c78d54792ecba4447bc
CARLOCAETANO <carlo.caetano-ext@ab-inbev.com>
2025-03-24 22:53:52 -0300

    Messaging module: Added Listener implementations

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/message/RestaurantApprovalResponse.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/message/RestaurantApprovalResponse.java
@@ -1,12 +1,10 @@
 package com.food.ordering.system.order.service.domain.dto.message;
 
 import com.food.ordering.system.domain.valueobject.OrderApprovalStatus;
-import com.food.ordering.system.domain.valueobject.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
 
@@ -17,7 +15,8 @@ public class RestaurantApprovalResponse {
     private String id;
     private String sagaId;
     private String orderId;
+    private String restaurantId;
     private Instant createdAt;
     private OrderApprovalStatus orderApprovalStatus;
-    private List<String> failureMessage;
+    private List<String> failureMessages;
 }
